13e85153d96e3447dd38f38e18ed89639fc3a230
szunami <sam.szuflita@gmail.com>
2022-10-17 11:58:16 -0400

    Add UserId resource

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::{net::TcpStream, collections::HashSet};
+use std::{collections::HashSet, net::TcpStream};
 
 use bevy::prelude::*;
-use jsonwebtoken::{decode, decode_header, Algorithm, DecodingKey, Validation};
+use jsonwebtoken::{decode, decode_header, Algorithm, DecodingKey, TokenData, Validation};
 use serde::{Deserialize, Serialize};
 use tungstenite::{connect, stream::MaybeTlsStream, Message, WebSocket};
 use url::Url;
@@ -22,12 +22,30 @@ struct Token {
     id: String,
 }
 
+fn decode_user_id_without_validating_jwt(
+    token: &str,
+) -> Result<TokenData<Token>, jsonwebtoken::errors::Error> {
+    let mut validation = Validation::new(Algorithm::HS256);
+    validation.insecure_disable_signature_validation();
+    validation.validate_exp = false;
+    validation.required_spec_claims = HashSet::new();
+
+    decode::<Token>(
+        token,
+        // Not verifying, we don't need a secret
+        &DecodingKey::from_secret("".as_ref()),
+        &validation,
+    )
+}
+
 fn login(app_id: &str) -> Result<LoginResponse, Box<dyn std::error::Error>> {
     let login_url = format!("https://coordinator.hathora.dev/{app_id}/login/anonymous");
     let client = reqwest::blocking::Client::new();
     let resp: LoginResponse = client.post(login_url).send()?.json()?;
     Ok(resp)
 }
+
+struct UserId(String);
 
 fn setup_websocket(mut commands: Commands) {
     let app_id = "e2d8571eb89af72f2abbe909def5f19bc4dad0cd475cce5f5b6e9018017d1f1c";
@@ -37,55 +55,21 @@ fn setup_websocket(mut commands: Commands) {
     let login_result = login(app_id);
     let login_response = login_result.expect("Logging in should succeed");
 
+    let user_id = decode_user_id_without_validating_jwt(&login_response.token)
+        .expect("Decoding JWT should succeed");
+
+    commands.insert_resource(UserId(user_id.claims.id));
+
     let websocket_url = format!("wss://coordinator.hathora.dev/connect/{app_id}");
 
-    dbg!("Attempting to connect to {}", websocket_url.clone());
-    let (mut socket, response) =
+    let (mut socket, _response) =
         connect(Url::parse(&websocket_url).unwrap()).expect("Can't connect");
-
-    dbg!("Connected to the server");
-    dbg!("Response HTTP code: {}", response.status());
-    dbg!("Response contains the following headers:");
-    for (ref header, _value) in response.headers() {
-        println!("* {}", header);
-    }
-
-    match decode_header(&login_response.token) {
-        Ok(ok) => {
-            dbg!("{}", ok);
-        }
-        Err(e) => {
-            dbg!("{}", e);
-        }
-    }
-
-    let mut validation = Validation::new(Algorithm::HS256);
-    validation.insecure_disable_signature_validation();
-    validation.validate_exp = false;
-    validation.required_spec_claims = HashSet::new();
-
-    match decode::<Token>(
-        &login_response.token,
-        &DecodingKey::from_base64_secret("".as_ref()).expect("asdf"),
-        &validation,
-    ) {
-        Ok(ok) => {
-            dbg!("{}", ok);
-        }
-        Err(e) => {
-            dbg!("{}", e);
-        }
-    }
-
-    dbg!("{}", login_response.to_owned());
 
     let initial_state = InitialState {
         token: login_response.token,
         stateId: room_id.to_owned(),
     };
-
     let message = serde_json::to_vec(&initial_state).expect("Serialization should work");
-
     match socket.write_message(Message::binary(message)) {
         Ok(_) => {
             dbg!("Successfully connected to websocket.");

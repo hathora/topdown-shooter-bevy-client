39b2a9ec0231669f038eb329af976c800dd3e336
szunami <sam.szuflita@gmail.com>
2022-10-19 12:50:25 -0400

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,12 @@
-use bevy::app::ScheduleRunnerPlugin;
-use bevy::core::CorePlugin;
-use bevy::input::InputPlugin;
-use bevy::time::TimePlugin;
-use bevy::window::WindowPlugin;
-use bevy::{prelude::*, render::camera, tasks::AsyncComputeTaskPool};
-use futures::future::Ready;
-use futures::{stream::StreamExt, AsyncBufReadExt, AsyncReadExt, AsyncWriteExt};
-use futures::{FutureExt, SinkExt};
-use pharos::*;
+use bevy::prelude::*;
+
+use futures::stream::StreamExt;
+use futures::SinkExt;
 use serde::{Deserialize, Serialize};
-use std::borrow::Borrow;
 use std::collections::HashSet;
 use std::iter::Iterator;
-use std::pin::Pin;
 use std::task::Context;
-use url::Url;
 use wasm_bindgen::prelude::*;
-use web_sys::WebSocket;
 use ws_stream_wasm::{WsMessage, WsMeta, WsStream};
 
 #[derive(Deserialize, Debug, Clone)]
@@ -65,78 +55,25 @@ async fn login(app_id: &str) -> Result<LoginResponse, Box<dyn std::error::Error>
 #[derive(Component, Eq, PartialEq, Hash, Clone, Debug)]
 struct UserId(String);
 
-fn setup_websocket(mut commands: Commands) {
+fn setup(mut commands: Commands) {
     commands.spawn_bundle(Camera2dBundle::default());
-
-    let app_id = "e2d8571eb89af72f2abbe909def5f19bc4dad0cd475cce5f5b6e9018017d1f1c";
-    let websocket_url = format!("wss://coordinator.hathora.dev/connect/{app_id}");
-    // let (mut socket, _response) =
-    //     connect(Url::parse(&websocket_url).unwrap()).expect("Can't connect");
-
-    // // // TODO: room should be dynamic
-    // let room_id = "2g80ygbukgn65";
-    // let app_id = "e2d8571eb89af72f2abbe909def5f19bc4dad0cd475cce5f5b6e9018017d1f1c";
-
-    // let thread_pool = AsyncComputeTaskPool::get();
-
-    // let x = thread_pool.spawn_local(async move {
-    //     web_sys::console::log_1(&"inside task".into());
-
-    // let login_result = login(app_id).await;
-    // let login_response = login_result.expect("Logging in should succeed");
-    // web_sys::console::log_1(&login_response.token.into());
-
-    // let user_id = decode_user_id_without_validating_jwt(&login_response.token)
-    // .expect("Decoding JWT should succeed");
-
-    // commands.insert_resource(UserId(user_id.claims.id));
-
-    // let initial_state = InitialState {
-    //     token: login_response.token,
-    //     stateId: room_id.to_owned(),
-    // };
-    // let message = serde_json::to_vec(&initial_state).expect("Serialization should work");
-    // match socket.write_message(Message::binary(message)) {
-    //     Ok(_) => {
-    //         dbg!("Successfully connected to websocket.");
-    //     }
-    //     Err(e) => {
-    //         dbg!("Failed to connect to websocket. Error was {}", e);
-    //     }
-    // }
-
-    // commands.insert_resource(socket);
 }
 
 #[wasm_bindgen]
 pub async fn run() {
-    dbg!("in the browser");
     let room_id = "374jv73032a1i";
     let app_id = "e2d8571eb89af72f2abbe909def5f19bc4dad0cd475cce5f5b6e9018017d1f1c";
     let login_result = login(app_id).await;
     let login_response = login_result.expect("Logging in should succeed");
-    web_sys::console::log_1(&"decoding user_id...".into());
 
     let user_id = decode_user_id_without_validating_jwt(&login_response.token)
         .expect("Decoding JWT should succeed");
-
-    web_sys::console::log_1(&"done decoding user_id".into());
-    web_sys::console::log_1(&user_id.clone().into());
-
     let app_id = "e2d8571eb89af72f2abbe909def5f19bc4dad0cd475cce5f5b6e9018017d1f1c";
     let websocket_url = format!("wss://coordinator.hathora.dev/connect/{app_id}");
 
-    log::info!("Connecting to websocket");
-
-    let (mut ws, mut ws_stream) = WsMeta::connect(websocket_url, None)
+    let (_ws, mut ws_stream) = WsMeta::connect(websocket_url, None)
         .await
         .expect_throw("assume the connection succeeds");
-
-    log::info!("Connected to websocket!");
-
-    // let mut io_stream = ws_stream.into_io();
-
-    // let (mut read, mut write) = io_stream.split();
 
     let initial_state = InitialState {
         token: login_response.token,
@@ -144,70 +81,20 @@ pub async fn run() {
     };
     let message = serde_json::to_vec(&initial_state).expect("Serialization should work");
 
-    log::info!("Writing to websocket.");
     let message = WsMessage::Binary(message);
 
     let send_result = ws_stream.send(message).await;
 
     match send_result {
-        Ok(_) => log::info!("Successfully wrote to websocket."),
-        Err(e) => log::error!("Failed to write to websocket; {}", e),
+        Ok(_) => debug!("Successfully wrote to websocket."),
+        Err(e) => error!("Failed to write to websocket; {}", e),
     }
-
-    // let mut output_1 = Vec::with_capacity(1024);
-
-    // let x = serde_json::from_reader(io_stream);
-
-    // todo: hook this into bevy lifecycle somehow
-
-    // let a = futures::executor::block_on(ws_stream.next());
-
-    log::info!("Reading from websocket.");
-    // while let Some(message) = ws_stream.next().await {
-    //     match message {
-    //         WsMessage::Text(t) => {
-    //             log::info!("Got text: {:#?}", t);
-    //             if !t.is_empty() {
-    //                 let x: UpdateMessage =
-    //                     serde_json::from_str(&t).expect("Successfully deserialized update");
-    //             }
-    //         }
-    //         WsMessage::Binary(b) => {
-    //             log::info!(
-    //                 "Got binary: {:#?}",
-    //                 String::from_utf8(b.clone()).expect("asdf")
-    //             );
-    //             if !b.is_empty() {
-    //                 let x: UpdateMessage =
-    //                     serde_json::from_slice(&b).expect("Successfully deserialized update");
-    //             }
-    //         }
-    //     }
-    // }
-
-    // let mut output_2 = Vec::with_capacity(1024);
-
-    // while let Ok(bytes) = io_stream.read_until(b'}', &mut output_2).await {
-    //     if bytes == 0 {
-    //         log::info!("Zero byte update");
-    //         // log::info!("Read from websocket: {:#?}", output_2);
-    //     } else {
-    //         log::info!("Read {} bytes from websocket.", bytes);
-    //         // log::info!("Read from websocket: {:#?}", output_2);
-    //     }
-    // }
-
-    // let mut evts = ws
-    //     .observe(ObserveConfig::default())
-    //     .await
-    //     .expect_throw("Observe");
-    // let x = evts.next().await.expect("First message");
 
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ws_stream)
         .insert_resource(UserId(user_id))
-        .add_startup_system(setup_websocket)
+        .add_startup_system(setup)
         .add_system(update_state)
         .run();
 }
@@ -250,42 +137,32 @@ fn poll_for_update(socket: &mut WsStream) -> Option<UpdateMessage> {
     let mut ctx = Context::from_waker(&waker);
 
     match socket.poll_next_unpin(&mut ctx) {
-        std::task::Poll::Ready(message) => {
-            log::info!("Message is ready");
-            match message {
-                Some(message) => match message {
-                    WsMessage::Text(t) => {
-                        log::info!("Got text: {:#?}", t);
-                        if !t.is_empty() {
-                            return Some(
-                                serde_json::from_str::<UpdateMessage>(&t)
-                                    .expect("Successfully deserialized update"),
-                            );
-                        }
-                        None
-                    }
-                    WsMessage::Binary(b) => {
-                        log::info!(
-                            "Got binary: {:#?}",
-                            String::from_utf8(b.clone()).expect("asdf")
+        std::task::Poll::Ready(message) => match message {
+            Some(message) => match message {
+                WsMessage::Text(t) => {
+                    if !t.is_empty() {
+                        return Some(
+                            serde_json::from_str::<UpdateMessage>(&t)
+                                .expect("Successfully deserialized update"),
                         );
-                        if !b.is_empty() {
-                            return Some(
-                                serde_json::from_slice::<UpdateMessage>(&b)
-                                    .expect("Successfully deserialized update"),
-                            );
-                        }
-                        return None;
                     }
-                },
-                None => {
-                    log::info!("Message was None");
+                    None
+                }
+                WsMessage::Binary(b) => {
+                    if !b.is_empty() {
+                        return Some(
+                            serde_json::from_slice::<UpdateMessage>(&b)
+                                .expect("Successfully deserialized update"),
+                        );
+                    }
                     return None;
                 }
+            },
+            None => {
+                return None;
             }
-        }
+        },
         std::task::Poll::Pending => {
-            log::info!("Still pending");
             return None;
         }
     }
@@ -319,21 +196,21 @@ fn update_state(
             spawned.insert(user_id.0.clone());
             for player in update.state.players.iter() {
                 if player.id == user_id.0 {
-                    log::info!("Updating {:?}", player.id);
+                    info!("Updating {:?}", player.id);
                     found = true;
                     player_transform.translation.x = player.position.x;
                     player_transform.translation.y = player.position.y;
                 }
             }
             if !found {
-                log::info!("Despawning {:?}", entity);
+                info!("Despawning {:?}", entity);
                 commands.entity(entity).despawn();
             }
         }
 
         for player in update.state.players.iter() {
             if !spawned.contains(&player.id) {
-                log::info!("Spawning {}", &player.id);
+                info!("Spawning {}", &player.id);
                 commands
                     .spawn()
                     .insert(UserId(player.id.clone()))
